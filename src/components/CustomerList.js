e3cd630b20fc9bb7505a399756be58f21498ede3
Samatha Sunkara <syeruva989@gmail.com>
2024-05-22 19:42:29 +0530

    Added ilookup froent end code

--- src/components/CustomerList.js
+++ src/components/CustomerList.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useState } from "react";
 import { Button, Modal, Table } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import AppContext from "./AppContext";
